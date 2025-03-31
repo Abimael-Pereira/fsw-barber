bd6b3c40f2430ee390753819b4d8953098340eb4
Abimael <abimael.spdev@gmail.com>
2025-03-30 22:29:15 -0400

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+
 const Home = () => {
-  return <h1>Home Page</h1>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Abimael</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[180px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
