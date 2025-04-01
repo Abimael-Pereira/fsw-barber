8f01cfeeb1792add82e9d8a92c36c964882015b6
Abimael <abimael.spdev@gmail.com>
2025-04-01 00:11:26 -0400

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -19,7 +22,7 @@ const Home = () => {
           </Button>
         </div>
 
-        <div className="relative mt-6 h-[180px] w-full">
+        <div className="relative mt-6 h-[100px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
             src="/banner-01.png"
@@ -27,6 +30,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                  <AvatarFallback>BS</AvatarFallback>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
